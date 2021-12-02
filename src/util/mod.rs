b51f1ff72b6c747339d66330899062766944f0ca
Steven Malis <smmalis37@gmail.com>
2021-12-02 03:01:58 -0500

    More cleanups.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,13 +1,15 @@
+#![allow(dead_code)]
+
 pub type HashMap<K, V> = ahash::AHashMap<K, V>;
 pub type HashSet<V> = ahash::AHashSet<V>;
 
 pub trait BStrParse {
-    fn parse<F: lexical::FromLexical>(&self) -> F;
+    fn parse<F: lexical_core::FromLexical>(&self) -> F;
 }
 
 impl BStrParse for [u8] {
-    fn parse<F: lexical::FromLexical>(&self) -> F {
-        lexical::parse(self).unwrap()
+    fn parse<F: lexical_core::FromLexical>(&self) -> F {
+        lexical_core::parse(self).unwrap()
     }
 }
 
